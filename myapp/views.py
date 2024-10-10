d99d6f25a0ad6896bcc6e54c87e18b8c8198729e
felixstiven <felixstiven12@gmail.com>
2024-10-10 14:59:53 -0500

    agregando codigo

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,8 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateNewTask
 
 
 # Create your views here.
@@ -21,17 +22,26 @@ def hello(request, username):
 def projects (request):
     #projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, 'projects.html', {
+    return render(request, 'projects/projects.html', {
         'projects' : projects
     })
 
 def tasks(request):
     #task = Task.objects.get(title = title)
     tasks = Task.objects.all()
-    return render(request, 'tasks.html', {
+    return render(request, 'tasks/tasks.html', {
         'tasks': tasks
     })
-
-
+    
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'tasks/create_task.html', {
+            'form': CreateNewTask()
+        }) 
+    else: 
+        Task.objects.create(title=request.POST['title'], descripcion=request.POST['descripcion'], project_id=1)
+        return redirect('/tasks/') 
+def create_project(request):
+    return render(request, 'projects/create_project.html')
 
 
