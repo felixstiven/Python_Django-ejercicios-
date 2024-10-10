4631c09a3a229a68e80c5389536f9c5a793c5508
felixstiven <felixstiven12@gmail.com>
2024-10-10 11:15:14 -0500

    actualizando codigo

--- myapp/models.py
+++ myapp/models.py
@@ -13,7 +13,8 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     descripcion = models.TextField()
     project =  models.ForeignKey(Project, on_delete=models.CASCADE)  
+    confirmation = models.BooleanField(default=False)
     
     def __str__(self):
         return self.title + '-' + self.project.name
- 
+
