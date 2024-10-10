6858bba5563174e9be59560c8bc4f71c3c7416e0
felixstiven <felixstiven12@gmail.com>
2024-10-10 11:46:29 -0500

    agragando funcionalidad

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path("", views.index ),
+    path("index", views.index ),
     path("about/", views.about),
     path('hello/<str:username>', views.hello),
     path("projects", views.projects),
